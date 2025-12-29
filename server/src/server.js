8586a045ea7563f16d713682de402e9192e5fc9d
huynh hoang <huynhhoang712003@gmail.com>
2025-12-29 18:17:51 +0700

    fix

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,27 @@
 const express = require('express')
-const app = express()
-const port = 3000
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
 const cookieParser = require('cookie-parser')
-const connectDB = require('./config/connectDB')
-const routes = require('./routes/index.routes')
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+const connectDB = require('../config/connectDB')
+const routes = require('../routes/index.routes')
+
+const app = express()
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // lát nữa sửa
+    credentials: true
+  })
+)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
+
 connectDB()
+
 app.get('/', (req, res) => {
-  return res.status(200).json({
+  res.status(200).json({
     success: true,
     message: 'Hello l2 team'
   })
@@ -24,7 +31,7 @@ routes(app)
 
 app.use((err, req, res, next) => {
   const statuscode = err.statusCode || 500
-  return res.status(statuscode).json({
+  res.status(statuscode).json({
     success: false,
     message: err.message || 'lỗi server'
   })
